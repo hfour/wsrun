d74628d3f0733d913e596e6bfec40a3eb5b5c255
Govind Varshney <Govind.Varshney@symphony.com>
2018-04-22 13:34:34 -0700

    Added help text to wsrun, taken from the readme

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,80 @@
 
 import * as Promise from 'bluebird'
 import * as fs from 'fs'
-import { argv } from 'yargs'
+import * as yargs from 'yargs'
 import * as _ from 'lodash'
 import chalk from 'chalk'
 
 import { RunGraph } from './parallelshell'
 import { listPkgs } from './workspace'
 
+yargs
+  .wrap(yargs.terminalWidth() - 1)
+  .usage('Usage: $0 <command> [<package>] [options]')
+  .group(['parallel', 'stages', 'serial'], 'Mode (choose one):')
+  .options({
+    'parallel': {
+      describe: 'Fully parallel mode (default)'
+    },
+    'stages': {
+      describe: 'Run in stages: start with packages that have no deps'
+    },
+    'serial': {
+      describe: 'Same as "stages" but with no parallelism at the stage level'
+    }
+  })
+  .group('recursive', 'Individual Package Options:')
+  .options({
+    'recursive': {
+      alias: 'r',
+      describe: 'Execute the same script on all of its dependencies, too'
+    }
+  })
+  .group([
+    'fast-exit',
+    'collect-logs',
+    'no-prefix',
+    'bin',
+    'done-criteria',
+    'exclude',
+    'exclude-missing',
+    'report'
+  ], 'Misc Options:')
+  .options({
+    'fast-exit': {
+      describe: 'If at least one script exits with code > 0, abort'
+    },
+    'collect-logs': {
+      describe: 'Collect per-package output and print it at the end of each script'
+    },
+    'no-prefix': {
+      describe: 'Don\'t prefix output'
+    },
+    'bin': {
+      default: 'yarn',
+      describe: 'The program to pass the command to'
+    },
+    'done-criteria': {
+      describe: 'Consider a process "done" when an output line matches the specified RegExp'
+    },
+    'exclude': {
+      type: 'string',
+      describe: 'Skip running the command for that package'
+    },
+    'exclude-missing': {
+      describe: 'Skip packages which lack the specified command in the scripts section of their package.json'
+    },
+    'report': {
+      describe: 'Show an execution report once the command has finished in each package'
+    }
+  })
+  .example('',
+    `wsrun clean - Will run "yarn clean" in each of the packages in parallel
+wsrun build --stages - Will run "yarn build" in each of the packages in stages, moving up the dependency tree
+wsrun watch app -r --stages --done-criteria='Compilation complete' - Will run "yarn watch" in app and all of its dependencies in stages, continuing when the process outputs "Compilation Complete."
+wsrun test --exclude-missing - Will run "yarn test" in all packages that have such a script`)
+
+const argv = yargs.argv
 const bin = argv.bin || 'yarn'
 
 let mode: string
@@ -41,7 +108,8 @@ const cmd = argv._[0]
 const pkgName = argv._[1]
 
 if (!cmd) {
-  throw new Error('cmd is undefined')
+  yargs.showHelp();
+  process.exit(1);
 }
 
 type BuildInstr = { name: string; order: number; cycle: boolean }
