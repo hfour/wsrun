4275aca00e625425ada75762740fb8a434a7a920
Ivan Vasilov <vasilov.ivan@gmail.com>
2019-11-12 12:16:12 +0100

    Separate the changedSince functionality in a separate function.

--- src/run-graph.ts
+++ src/run-graph.ts
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 
 import { PkgJson, Dict } from './workspace'
 import { ResultSpecialValues, Result, ProcResolution } from './enums'
-import { uniq, values } from 'lodash'
+import { uniq } from 'lodash'
 import { CmdProcess } from './cmd-process'
 import minimatch = require('minimatch')
 import { fixPaths } from './fix-paths'
@@ -307,45 +307,45 @@ export class RunGraph {
     return pkgsInError.length > 0
   }
 
-  expandGlobs(globs: string[]): Promise<string[]> {
+  async expandGlobs(globs: string[]) {
     let pkgs = this.pkgJsons
       .map(p => p.name)
       .filter(name => globs.some(glob => minimatch(name, glob)))
 
     // if changedSince is defined, filter the packages to contain only changed packages (according to git)
     if (this.opts.changedSince) {
-      return getChangedFilesForRoots([this.opts.workspacePath], {
-        changedSince: this.opts.changedSince
-      }).then(data => {
-        if (!data.repos || (data.repos.git.size === 0 && data.repos.hg.size === 0)) {
-          throw new Error(
-            "The workspace is not a git/hg repo and it cannot work with 'changedSince'"
-          )
-        }
-
-        /**
-         * filter the packages by checking if they have any changed files. This way is quicker
-         * (mapping over packages) because package count is usually lower than changed files count
-         * and we only need to check once per package.
-         */
-        pkgs = pkgs.filter(pkg => {
-          const pkgPath = this.pkgPaths[pkg]
-          const path = `${this.opts.workspacePath}/${pkgPath}`
-
-          for (const file of data.changedFiles.values()) {
-            if (file.startsWith(path)) {
-              return true
-            }
-          }
-          return false
-        })
-
-        return pkgs
-      })
-    } else {
-      Promise.resolve(pkgs)
+      pkgs = await this.filterChangedPackages(pkgs)
     }
-    return Promise.resolve([])
+    return Promise.resolve(pkgs)
+  }
+
+  filterChangedPackages(pkgs: string[]) {
+    return getChangedFilesForRoots([this.opts.workspacePath], {
+      changedSince: this.opts.changedSince
+    }).then(data => {
+      if (!data.repos || (data.repos.git.size === 0 && data.repos.hg.size === 0)) {
+        throw new Error("The workspace is not a git/hg repo and it cannot work with 'changedSince'")
+      }
+
+      /**
+       * filter the packages by checking if they have any changed files. This way is quicker
+       * (mapping over packages) because package count is usually lower than changed files count
+       * and we only need to check once per package.
+       */
+      pkgs = pkgs.filter(pkg => {
+        const pkgPath = this.pkgPaths[pkg]
+        const path = `${this.opts.workspacePath}/${pkgPath}`
+
+        for (const file of data.changedFiles.values()) {
+          if (file.startsWith(path)) {
+            return true
+          }
+        }
+        return false
+      })
+
+      return pkgs
+    })
   }
 
   async run(cmd: string[], globs: string[] = ['**/*']) {
